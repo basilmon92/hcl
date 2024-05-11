0ce9c574792f11307d33d5a8993ad2f08e8ffb93
chidananda-mn <17chidananda@gmail.com>
2024-05-11 15:50:40 +0530

    added UI

--- src/app/components/input/index.tsx
+++ src/app/components/input/index.tsx
@@ -1,9 +1,12 @@
 'use client'
 import { useState } from 'react'
 import { TextField } from '@mui/material';
-export default function inputBox() {
+export default function InputBox(props: any) {
 
 const [input, setInput] = useState('')
+const changeHandler = ()=> {
+    props.onInputHandler(input)
+}
 return (
-    <TextField type="number" onChange={(e: any)=>setInput(e.target.value)} />)
+    <TextField type="number" value={input} onChange={(e: any)=>setInput(e.target.value)} onInput={changeHandler} />)
 }
--- src/app/components/select/index.tsx
+++ src/app/components/select/index.tsx
@@ -2,12 +2,15 @@
 import { useState } from 'react'
 import Select from '@mui/material/Select';
 
-export default function selectComponent() {
+export default function DropDown(props: any) {
     const [select, setSelect] = useState('credits');
+    const changeHandler = ()=> {
+        props.onInputHandler(select)
+    }
     return (
       <label>
         Payment Type
-        <Select value={select} onChange={e => setSelect(e.target.value)} name="select">
+        <Select value={select} onChange={e => setSelect(e.target.value)} name="select" onInput={changeHandler}>
           <option value="credits">Credits</option>
           <option value="debits">Debits</option>
         </Select>
