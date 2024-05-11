ca0ddef377c52314a1e547bbd4bfcaab79c2bc5c
chidananda-mn <17chidananda@gmail.com>
2024-05-11 15:32:00 +0530

    added material UI

--- src/app/components/input/index.tsx
+++ src/app/components/input/index.tsx
@@ -1,9 +1,9 @@
 'use client'
 import { useState } from 'react'
+import { TextField } from '@mui/material';
 export default function inputBox() {
 
 const [input, setInput] = useState('')
-return (<>
-    <input type="number" onChange={(e: any)=>setInput(e.target.value)} />
-</>)
+return (
+    <TextField type="number" onChange={(e: any)=>setInput(e.target.value)} />)
 }
--- src/app/components/select/index.tsx
+++ src/app/components/select/index.tsx
@@ -1,14 +1,16 @@
 'use client'
 import { useState } from 'react'
+import Select from '@mui/material/Select';
+
 export default function selectComponent() {
     const [select, setSelect] = useState('credits');
     return (
       <label>
         Payment Type
-        <select value={select} onChange={e => setSelect(e.target.value)} name="select">
+        <Select value={select} onChange={e => setSelect(e.target.value)} name="select">
           <option value="credits">Credits</option>
           <option value="debits">Debits</option>
-        </select>
+        </Select>
       </label>
     );
   }
