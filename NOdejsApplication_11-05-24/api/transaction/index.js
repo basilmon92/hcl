d23afa99e1d44c598d12810ef8c0dcfe77140829
Shambhu260 <shambhu2892@gmail.com>
2024-05-11 14:59:47 +0530

    Remove api folder

--- NOdejsApplication_11-05-24/api/transaction/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const controller = require("./transaction.controller")
-const router = express.Router();
-var verifyToken = require('../auth/auth');
-router.post("/login", controller.login)
-router.post("/register",controller.register)
-router.get("/getTransactionById/:transactionId", controller.getTransactionById)
-module.exports = router 
--- NOdejsApplication_11-05-24/api/transaction/transaction.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-const transactionSchema = new mongoose.Schema({
-    customerId: {type: String,default: "ABC001"},
-    transactionDescription: {type: String},
-    amount: {type: Number},
-    paymentType: {type: String, default: "credit"},
-    dateTime: {type: Date, required: true, default: Date.now()},
-})
-module.exports = mongoose.model("transaction", transactionSchema, "transaction")
